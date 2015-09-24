ffde1667dfa7edf4c94218b79f8907303f8292c2
amarkpark <amarkpark@gmail.com>
2015-09-24 17:59:54 +0000

    Flixter Lesson 2: Commit Prime. ~AMP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flixter::Application.config.secret_key_base = '816253076e20b0ed2b8eec4b39e1d85019941ad92c652e7e0c1f2b89e8b8426059f44e61460b9434dad109d7ef49ec437767fdc590efcbe883891a1dd5257f71'
