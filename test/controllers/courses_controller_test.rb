52927239ef9faca14a7a5a860678ad0006bdf678
amarkpark <amarkpark@gmail.com>
2015-10-14 00:45:07 +0000

    mid-Lesson 15: some PEBKAC test QAing, styling and features. ~AMP

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -7,4 +7,12 @@ class CoursesControllerTest < ActionController::TestCase
 		get :index
 		assert_response :success
 	end
+
+	test "show course detail" do
+		user = FactoryGirl.create(:user)
+		course = FactoryGirl.create(:course, :user_id => user.id)
+		get :show, :id => course.id
+		# assert_redirected_to course_path(course)
+		assert_response :success
+	end
 end
