160c6e81e99c23b304fa36707611a7110030e51e
amarkpark <nospam@amarkpark.com>
2015-11-07 23:54:32 +0000

    Lesson 27: ADDED ENROLLMENT VALIDATION WITHOUT LOOKING! ~AMP

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrolled_for_show_lesson
+
 	def show
 	end
 
@@ -8,4 +11,11 @@ class LessonsController < ApplicationController
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
+
+	def require_enrolled_for_show_lesson
+		unless current_user && current_user.enrolled_in?(current_lesson.course)
+			redirect_to course_path(current_lesson.course), flash: {alert: "You must be enrolled to view lessons!"} 
+		end
+	end
+
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,5 @@
 class Lesson < ActiveRecord::Base
 	belongs_to :section
+	has_one :course, through: :section
 	mount_uploader :video, VideoUploader
 end
