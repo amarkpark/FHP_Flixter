0bc916bc358d14a46aabf998ca7c4acb41d81294
amarkpark <nospam@amarkpark.com>
2015-11-19 06:21:40 +0000

    Lesson 35 complete: leaving new.html.erb to experiment! ~AMP

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Flixter::Application.routes.draw do
   resource :dashboard, :only => [:show]
   root "static_pages#index"
   resources :courses, :only => [:index, :show] do
-    resources :enrollments, :only => :create
+    resources :enrollments, :only => [:create]
   end
   resources :lessons, :only =>[:show]
   namespace :instructor do
     resources :lessons, :only => [:update]
     resources :sections, :only => [:update] do
-      resources :lessons, :only => [:new, :create]
+      resources :lessons, :only => [:create]
     end
     resources :courses, :only => [:new, :create, :show] do
       resources :sections, :only => [:create]
